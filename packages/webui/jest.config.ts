2f073f1b5d79ef241e8e91d5cf542bf699a9258e
Kevin Paxton <kevin.paxton@formidable.com>
2023-09-19 18:03:22 +0100

    Added mock data and CollectorClient unit tests

--- packages/webui/jest.config.ts
+++ packages/webui/jest.config.ts
@@ -7,4 +7,12 @@ export default {
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1',
   },
+  collectCoverageFrom: [
+    './src/**/*.{ts,tsx}',
+    '!**/*.d.ts',
+    '!**/node_modules/**',
+    '!**/dist/**',
+    '!**/bin/**',
+    '!**/coverage/**',
+  ],
 };
