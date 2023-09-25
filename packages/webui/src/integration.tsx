1c90b61bc877aa13036889c0732c911eac154dad
Kevin Paxton <kevin.paxton@formidable.com>
2023-09-25 11:09:35 +0100

    Added integration entry point to allow apps to define and register systems

---
+++ packages/webui/src/integration.tsx
@@ -0,0 +1,27 @@
+/* eslint-disable import/no-unresolved */
+// @ts-expect-error
+import css from 'inline:../dist/viewer.css';
+
+import App from './App';
+import { registerSystem } from './systems/registration';
+import { System } from './types';
+
+export type { System, TraceRowData, Trace } from './types';
+export * from './components';
+
+export type EnvyViewerProps = {
+  systems?: System<unknown>[];
+};
+
+export default function EnvyViewer({ systems }: EnvyViewerProps) {
+  for (const system of systems ?? []) {
+    registerSystem(system);
+  }
+
+  return (
+    <>
+      <style>{css}</style>
+      <App />
+    </>
+  );
+}
---
+++ packages/webui/src/scripts/buildIntegration.cjs
@@ -0,0 +1,30 @@
+const { build } = require('esbuild');
+const inlineImportPlugin = require('esbuild-plugin-inline-import');
+
+const { dependencies } = require('../../package.json');
+
+const integrationFile = 'src/integration.tsx';
+
+const shared = {
+  bundle: true,
+  entryPoints: [integrationFile],
+  external: Object.keys(dependencies),
+  logLevel: 'info',
+  plugins: [inlineImportPlugin()],
+  minify: true,
+  sourcemap: true,
+};
+
+build({
+  ...shared,
+  format: 'esm',
+  outfile: './dist/integration.esm.js',
+  target: ['esnext', 'node12.22.0'],
+});
+
+build({
+  ...shared,
+  format: 'cjs',
+  outfile: './dist/integration.cjs.js',
+  target: ['esnext', 'node12.22.0'],
+});
