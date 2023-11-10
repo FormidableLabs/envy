9531b8b91990205630f406afe53ca9961fca8ca3
Charlie Brown <carbonrobot@gmail.com>
2023-11-10 10:24:59 -0600

    Add nested multi level folding

--- packages/webui/src/components/MonacoEditor.tsx
+++ packages/webui/src/components/MonacoEditor.tsx
@@ -1,4 +1,5 @@
 import { Editor, EditorProps, OnMount } from '@monaco-editor/react';
+import { useEffect, useRef } from 'react';
 import colors from 'tailwindcss/colors';
 
 export type MonacoEditorProps = Pick<EditorProps, 'value' | 'language'>;
@@ -13,20 +14,37 @@ const editorOptions: EditorProps['options'] = {
   lineNumbers: 'off',
 };
 
-const setEditorTheme: OnMount = (editor, monaco) => {
-  monaco.editor.defineTheme('envy', {
-    base: 'vs',
-    inherit: true,
-    colors: {
-      'editor.background': colors.gray['200'],
-      'editor.lineHighlightBackground': colors.gray['200'],
-    },
-    rules: [],
-  });
-  monaco.editor.setTheme('envy');
-  editor.trigger('fold', 'editor.foldLevel4', {});
-};
-
 export default function MonacoEditor({ value, language }: MonacoEditorProps) {
-  return <Editor value={value} language={language} options={editorOptions} onMount={setEditorTheme} />;
+  const editorRef = useRef<Parameters<OnMount>['0'] | null>(null);
+
+  const executeFolding = () => {
+    if (!editorRef.current) return;
+
+    // fold deeply nested objects
+    editorRef.current?.trigger('fold', 'editor.foldLevel7', {});
+    editorRef.current?.trigger('fold', 'editor.foldLevel6', {});
+    editorRef.current?.trigger('fold', 'editor.foldLevel5', {});
+    editorRef.current?.trigger('fold', 'editor.foldLevel4', {});
+  };
+
+  const onMount: OnMount = (editor, monaco) => {
+    editorRef.current = editor;
+
+    monaco.editor.defineTheme('envy', {
+      base: 'vs',
+      inherit: true,
+      colors: {
+        'editor.background': colors.gray['200'],
+        'editor.lineHighlightBackground': colors.gray['200'],
+      },
+      rules: [],
+    });
+    monaco.editor.setTheme('envy');
+
+    executeFolding();
+  };
+
+  useEffect(executeFolding, [value, language]);
+
+  return <Editor value={value} language={language} options={editorOptions} onMount={onMount} />;
 }
