3cbb2f9ade5b07f733e84bf875d13056c4264df7
Ryan Srofe <ryan.srofe@formidable.com>
2023-11-14 05:55:24 -0500

    adding gap, padding, radius, and space theme variables based on figma designs
    the name is based on the pixel value that the rem value represents, the same names and values are used in Figma designs.

--- packages/webui/tailwind.config.js
+++ packages/webui/tailwind.config.js
@@ -3,6 +3,41 @@ export default {
   darkMode: ['class'],
   content: ['./src/index.html', './src/**/*.tsx'],
   theme: {
+    gap: {
+      0: '0px',
+      4: '0.25rem',
+      8: '0.5rem',
+      12: '0.75rem',
+      16: '1rem',
+      24: '1.5rem',
+    },
+    padding: {
+      0: '0px',
+      2: '0.125rem',
+      4: '0.25rem',
+      6: '0.375rem',
+      8: '0.5rem',
+      12: '0.75rem',
+      16: '1rem',
+    },
+    radius: {
+      0: '0px',
+      2: '0.125rem',
+      4: '0.25rem',
+      8: '0.5rem',
+      16: '1rem',
+    },
+    space: {
+      0: '0px',
+      1: '1px',
+      6: '0.375rem',
+      24: '1.5rem',
+      48: '3rem',
+      96: '6rem',
+      192: '12rem',
+      240: '15rem',
+      320: '20rem',
+    },
     extend: {
       fontSize: {
         '2xs': ['0.6rem', '0.9rem'],
