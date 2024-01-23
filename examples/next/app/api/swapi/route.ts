99c636268e2496bb1696cc76fe0b8b25381cbf4e
Kevin Paxton <kevin.paxton@formidable.com>
2024-01-23 16:24:01 +0000

    Demonstration of duplicate traces showing in Envy

---
+++ examples/next/app/api/swapi/route.ts
@@ -0,0 +1,10 @@
+import axios from 'axios';
+import { NextResponse } from 'next/server';
+
+export const SWAPI_URL = 'https://swapi.dev/api/people/1';
+
+export async function GET() {
+  // Look - just one call to the SWAPI endpoint!
+  const { data } = await axios.get(SWAPI_URL);
+  return NextResponse.json(data);
+}
---
+++ examples/next/app/next13app/bug/page.tsx
@@ -0,0 +1,5 @@
+import BugExample from './BugExample';
+
+export default function Home() {
+  return <BugExample />;
+}
