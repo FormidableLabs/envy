58bcec56c64558fffa602840b2ddf299c1dc3a2b
Nathan Kluth <nlkluth@gmail.com>
2025-01-08 11:01:45 -0700

    swapi moved

--- examples/next/app/api/swapi/route.ts
+++ examples/next/app/api/swapi/route.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { NextResponse } from 'next/server';
 
-const SWAPI_URL = 'https://swapi.dev/api/people/1';
+const SWAPI_URL = 'https://swapi.py4e.com/api/people/1';
 
 export async function GET() {
   // Look - just one call to the SWAPI endpoint!
